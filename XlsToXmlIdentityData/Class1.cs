5a487b2e07ff69277e9e85b3cf704271fcf24895
Шаповалов Александр Сергеевич <alshapovalov@sfedu.ru>
2022-02-13 23:38:17 +0300

    Добавьте файлы проекта.

---
+++ XlsToXmlIdentityData/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XlsToXmlIdentityData
+{
+    class PersonIdentity
+    {
+        public string fullName { get; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string middleName { get; set; }
+        public string birthDate { get; set; }
+
+        public PersonIdentity(string firstName, string lastName, string middleName, string birthDate)
+        {
+            this.fullName = getFullName();
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.birthDate = birthDate;
+        }
+
+        public PersonIdentity(string fullName, string birthDate)
+        {
+            string[] Names =  getPartNames(fullName);
+            this.fullName = fullName;
+            this.lastName = Names[0];
+            this.firstName = Names[1];
+            try { 
+            this.middleName = Names[2];
+            }
+            catch
+            {
+                Console.WriteLine("No middle name");
+                this.middleName = "";
+            }
+            this.birthDate = birthDate;
+        }
+
+        private string getFullName()
+        {
+            return "" + this.lastName + this.firstName + this.middleName;
+        }
+
+        private string[] getPartNames(string fullName)
+        {
+            return fullName.Split(' ');
+        }
+
+    }
+
+    /*class Date
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public Date() 
+        {
+            this.Day = Day;
+            this.Month = Month;
+            this.Year = Year;
+        }
+    }*/
+}
---
+++ XlsToXmlIdentityData/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OfficeOpenXml;
+
+namespace XlsToXmlIdentityData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var package = new ExcelPackage(new System.IO.FileInfo(@"C:\Users\Admi\source\repos\XlsToXmlIdentityData\XlsToXmlIdentityData\Identity.xlsx"));
+            var worksheets = package.Workbook.Worksheets;
+            var worksheet = worksheets[0];
+            var lastRow = worksheet.Dimension.End.Row;
+            PersonIdentity[] person = new PersonIdentity[lastRow - 1];
+
+            for (int counter = 1; counter < lastRow; counter++)
+            {
+                try
+                {
+                    string FIO = worksheet.Cells[counter + 1, 1].Value.ToString();
+                    string birthDate = worksheet.Cells[counter + 1, 5].Value.ToString();
+                    person[counter-1] = new PersonIdentity(FIO, birthDate);
+                }
+                catch(NullReferenceException ex)
+                {
+                    Console.WriteLine("No birthdate in " + counter + " row");
+                }
+                
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
