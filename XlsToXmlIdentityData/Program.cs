729d5c2e048a9c86af1c33d2fd9fe61ff03dacef
Slamotin <daromir09@gmail.com>
2022-02-14 16:45:03 +0300

    Create/save xml function

--- XlsToXmlIdentityData/Program.cs
+++ XlsToXmlIdentityData/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using OfficeOpenXml;
+using System.Xml;
 
 namespace XlsToXmlIdentityData
 {
@@ -8,27 +9,39 @@ namespace XlsToXmlIdentityData
         static void Main(string[] args)
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            var package = new ExcelPackage(new System.IO.FileInfo(@"C:\Users\Admi\source\repos\XlsToXmlIdentityData\XlsToXmlIdentityData\Identity.xlsx"));
+            var package = new ExcelPackage(new System.IO.FileInfo(@"Identity.xlsx"));
             var worksheets = package.Workbook.Worksheets;
             var worksheet = worksheets[0];
             var lastRow = worksheet.Dimension.End.Row;
             PersonIdentity[] person = new PersonIdentity[lastRow - 1];
 
-            for (int counter = 1; counter < lastRow; counter++)
+            for (int curRow = 1; curRow < lastRow; curRow++)
             {
                 try
                 {
-                    string FIO = worksheet.Cells[counter + 1, 1].Value.ToString();
-                    string birthDate = worksheet.Cells[counter + 1, 5].Value.ToString();
-                    person[counter-1] = new PersonIdentity(FIO, birthDate);
+                    string FIO = worksheet.Cells[curRow + 1, 1].Value.ToString();
+                    string birthDate = worksheet.Cells[curRow + 1, 5].Value.ToString();
+                    person[curRow - 1] = new PersonIdentity(FIO, birthDate);
                 }
                 catch(NullReferenceException ex)
                 {
-                    Console.WriteLine("No birthdate in " + counter + " row");
+
+                    Console.WriteLine("No birthdate in " + curRow + " row");
                 }
                 
                 
             }
+
+            XmlDocument xmlDocument = new XmlDocument();
+            //XmlElement peoples = new XmlElement("peoples");
+
+
+
+            xmlDocument.Save("example.xml");
+
+
+
+
             Console.ReadKey();
         }
     }
